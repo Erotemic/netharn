6602456f221ab941c2a8e75d6098f5586f04fc28
Jon Crall <jon.crall@kitware.com>
2018-04-01 22:33:55 -0400

    wip

--- netharn/schedulers/listed.py
+++ netharn/schedulers/listed.py
@@ -22,26 +22,33 @@ class ListedLR(_LRScheduler2):
         step_points (dict): Mapping from epoch number to a learning rate
         last_epoch (int): The index of last epoch. Default: -1.
 
+    CommandLine:
+        python ~/code/netharn/netharn/schedulers/listed.py ListedLR
+
     Example:
         >>> # Assuming optimizer has two groups.
-        >>> from clab.lr_scheduler import *
-        >>> import torchvision
-        >>> model = torchvision.models.SqueezeNet()
+        >>> import ubelt as ub
+        >>> import netharn as nh
+        >>> model = nh.models.ToyNet2d()
         >>> optimizer = torch.optim.SGD(model.parameters(), lr=0)
         >>> step_points = {0: .01, 1: .02, 2: .1, 6: .05, 9: .025}
         >>> self = ListedLR(optimizer, step_points)
         >>> lrs = [self._get_epoch_lr(epoch) for epoch in range(-1, 11)]
-        >>> import ubelt as ub
         >>> print(list(ub.flatten(lrs)))
         [0, 0.01, 0.02, 0.1, 0.1, 0.1, 0.1, 0.05, 0.05, 0.05, 0.025, 0.025]
         >>> assert self.current_lrs() == [0.01]
-        >>> assert self.base_lrs() == [0]
+        >>> self = ListedLR(optimizer, step_points, interpolate=True)
+        >>> lrs = [self._get_epoch_lr(epoch) for epoch in range(-1, 11)]
+        >>> print(ub.repr2(list(ub.flatten(lrs)), precision=3, nl=0))
+        [0.027, 0.010, 0.020, 0.100, 0.062, 0.075, 0.088, 0.050, 0.033, 0.042, 0.025, 0.025]
     """
 
-    def __init__(self, optimizer, step_points, last_epoch=-1):
+    def __init__(self, optimizer, step_points, interpolate=False,
+                 last_epoch=-1):
         self.optimizer = optimizer
         if not isinstance(step_points, dict):
             raise TypeError(step_points)
+        self.interpolate = interpolate
         self.step_points = step_points
         self.last_epoch = last_epoch
         # epochs where the lr changes
@@ -51,24 +58,50 @@ class ListedLR(_LRScheduler2):
     def get_lr(self):
         return self._get_epoch_lr(self.last_epoch)
 
-    # def step(self):
-    #     print('STEPING = {!r}'.format(self))
-    #     super().step()
-
     def _get_epoch_lr(self, epoch):
         """ return lr based on the epoch """
         key_epochs = self.key_epochs
         step_points = self.step_points
         base_lrs = self.base_lrs
 
+        # if epoch < 0:
+        #     epoch = 0
+
         if epoch in key_epochs:
-            key_epoch = epoch
+            prev_key_epoch = epoch
+            next_key_epoch = epoch
         else:
             idx = np.searchsorted(key_epochs, epoch, 'left') - 1
-            key_epoch = key_epochs[idx]
-        if epoch < key_epoch:
-            epoch_lrs = base_lrs
-        else:
-            new_lr = step_points[key_epoch]
+            prev_key_epoch = key_epochs[idx]
+            if idx < len(key_epochs) - 1:
+                next_key_epoch = key_epochs[idx + 1]
+            else:
+                next_key_epoch = prev_key_epoch
+
+        if self.interpolate:
+            if next_key_epoch == prev_key_epoch:
+                new_lr = step_points[next_key_epoch]
+            else:
+                prev_lr = step_points[next_key_epoch]
+                next_lr = step_points[prev_key_epoch]
+
+                alpha = (epoch - prev_key_epoch) / (next_key_epoch - prev_key_epoch)
+
+                new_lr = alpha * prev_lr + (1 - alpha) * next_lr
+
             epoch_lrs = [new_lr for _ in base_lrs]
+        else:
+            if epoch < prev_key_epoch:
+                epoch_lrs = base_lrs
+            else:
+                new_lr = step_points[prev_key_epoch]
+                epoch_lrs = [new_lr for _ in base_lrs]
         return epoch_lrs
+
+if __name__ == '__main__':
+    """
+    CommandLine:
+        python -m netharn.schedulers.listed all
+    """
+    import xdoctest
+    xdoctest.doctest_module(__file__)
---
+++ netharn/util/util_boxes.py
@@ -0,0 +1,223 @@
+import numpy as np
+import torch
+import ubelt as ub
+
+
+def scale_boxes(boxes, factor):
+    """
+    works with tlbr, cxywh, xywh, xy, or wh formats
+
+    Example:
+        >>> scale_boxes(np.array([1, 1, 10, 10]), 2)
+        array([ 2.,  2., 20., 20.])
+        >>> scale_boxes(np.array([[1, 1, 10, 10]]), (2, .5))
+        array([[ 2. ,  0.5, 20. ,  5. ]])
+        >>> scale_boxes(np.array([[10, 10]]), .5)
+        array([[5., 5.]])
+    """
+    sx, sy = factor if ub.iterable(factor) else (factor, factor)
+    if torch.is_tensor(boxes):
+        boxes = boxes.float().clone()
+    else:
+        boxes = boxes.astype(np.float).copy()
+    boxes[..., 0:4:2] *= sx
+    boxes[..., 1:4:2] *= sy
+    return boxes
+
+
+class Boxes(ub.NiceRepr):
+    """
+    Converts boxes between different formats as long as the last dimension
+    contains 4 coordinates and the format is specified.
+
+    Example:
+        >>> Boxes([25, 30, 15, 10], 'xywh')
+        <Boxes(xywh, array([25, 30, 15, 10]))>
+        >>> Boxes([25, 30, 15, 10], 'xywh').as_xywh()
+        <Boxes(xywh, array([25, 30, 15, 10]))>
+        >>> Boxes([25, 30, 15, 10], 'xywh').as_cxywh()
+        <Boxes(cxywh, array([32.5, 35. , 15. , 10. ]))>
+        >>> Boxes([25, 30, 15, 10], 'xywh').as_tlbr()
+        <Boxes(tlbr, array([25, 30, 40, 40]))>
+        >>> Boxes([25, 30, 15, 10], 'xywh').scale(2).as_tlbr()
+        <Boxes(tlbr, array([50., 60., 80., 80.]))>
+        >>> Boxes(torch.FloatTensor([[25, 30, 15, 20]]), 'xywh').scale(.1).as_tlbr()
+        <Boxes(tlbr,
+             2.5000  3.0000  4.0000  5.0000
+            [torch.FloatTensor of size (1,4)]
+            )>
+
+    Example:
+        >>> datas = [
+        >>>     [1, 2, 3, 4],
+        >>>     [[1, 2, 3, 4], [4, 5, 6, 7]],
+        >>>     [[[1, 2, 3, 4], [4, 5, 6, 7]]],
+        >>> ]
+        >>> formats = ['xywh', 'cxywh', 'tlbr']
+        >>> for format1 in formats:
+        >>>     for data in datas:
+        >>>         self = box1 = Boxes(data, format1)
+        >>>         for format2 in formats:
+        >>>             box2 = box1.asformat(format2)
+        >>>             back = box2.asformat(format1)
+        >>>             assert box1 == back
+    """
+    def __init__(self, data, format='xywh'):
+        if isinstance(data, (list, tuple)):
+            data = np.array(data)
+        self.data = data
+        self.format = format
+
+    def __eq__(self, other):
+        return np.all(self.data == other.data) and self.format == other.format
+
+    def __nice__(self):
+        # return self.format + ', shape=' + str(list(self.data.shape))
+        data_repr = repr(self.data)
+        if '\n' in data_repr:
+            data_repr = ub.indent('\n' + data_repr.lstrip('\n'), '    ')
+        return '{}, {}'.format(self.format, data_repr)
+
+    __repr__ = ub.NiceRepr.__str__
+
+    def copy(self):
+        new_data = self.data.copy()
+        return Boxes(new_data, self.format)
+
+    def scale(self, factor):
+        new_data = scale_boxes(self.data, factor)
+        return Boxes(new_data, self.format)
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def area(self):
+        w, h = self.as_xywh().components[-2:]
+        return w * h
+
+    @property
+    def components(self):
+        a = self.data[..., 0:1]
+        b = self.data[..., 1:2]
+        c = self.data[..., 2:3]
+        d = self.data[..., 3:4]
+        return [a, b, c, d]
+
+    def asformat(self, format):
+        if format == 'xywh':
+            return self.as_xywh()
+        elif format == 'tlbr':
+            return self.as_tlbr()
+        elif format == 'cxywh':
+            return self.as_cxywh()
+        else:
+            raise KeyError(self.format)
+
+    def _cat(self, datas):
+        if torch.is_tensor(datas[0]):
+            return torch.cat(datas, dim=-1)
+        else:
+            return np.concatenate(datas, axis=-1)
+
+    def as_xywh(self):
+        if self.format == 'xywh':
+            return self.copy()
+        elif self.format == 'tlbr':
+            x1, y1, x2, y2 = self.components
+            w = x2 - x1
+            h = y2 - y1
+        elif self.format == 'cxywh':
+            cx, cy, w, h = self.components
+            x1 = cx - w / 2
+            y1 = cy - h / 2
+        else:
+            raise KeyError(self.format)
+        xywh = self._cat([x1, y1, w, h])
+        return Boxes(xywh, 'xywh')
+
+    def as_cxywh(self):
+        if self.format == 'cxywh':
+            return self.copy()
+        elif self.format == 'tlbr':
+            x1, y1, x2, y2 = self.components
+            w = x2 - x1
+            h = y2 - y1
+            cx = (x1 + x2) / 2
+            cy = (y1 + y2) / 2
+        elif self.format == 'xywh':
+            x1, y1, w, h = self.components
+            cx = x1 + (w / 2)
+            cy = y1 + (h / 2)
+        else:
+            raise KeyError(self.format)
+        cxywh = self._cat([cx, cy, w, h])
+        return Boxes(cxywh, 'cxywh')
+
+    def as_tlbr(self):
+        if self.format == 'tlbr':
+            return self.copy()
+        elif self.format == 'cxywh':
+            cx, cy, w, h = self.components
+            half_w = (w / 2)
+            half_h = (h / 2)
+            x1 = cx - half_w
+            x2 = cx + half_w
+            y1 = cy - half_h
+            y2 = cy + half_h
+        elif self.format == 'xywh':
+            x1, y1, w, h = self.components
+            x2 = x1 + w
+            y2 = y1 + h
+        tlbr = self._cat([x1, y1, x2, y2])
+        return Boxes(tlbr, 'tlbr')
+
+
+def clip_boxes(boxes, im_shape):
+    """
+    Clip boxes to image boundaries inplace.
+
+    Args:
+        boxes (ndarray): multiple boxes in tlbr format
+        im_shape (tuple): (H, W) of original image
+
+    Example:
+        >>> boxes = np.array([[-10, -10, 120, 120], [1, -2, 30, 50]])
+        >>> im_shape = (100, 110)  # H, W
+        >>> clip_boxes(boxes, im_shape)
+        array([[  0,   0, 109,  99],
+               [  1,   0,  30,  50]])
+    """
+    if boxes.shape[0] == 0:
+        return boxes
+
+    if not isinstance(boxes, (np.ndarray, list)):
+        raise TypeError('got boxes={!r}'.format(boxes))
+
+    im_h, im_w = im_shape
+    x1, y1, x2, y2 = boxes.T
+    np.minimum(x1, im_w - 1, out=x1)  # x1 >= 0
+    np.minimum(y1, im_h - 1, out=y1)  # y1 >= 0
+    np.minimum(x2, im_w - 1, out=x2)  # x2 < im_shape[1]
+    np.minimum(y2, im_h - 1, out=y2)  # y2 < im_shape[0]
+    boxes = np.maximum(boxes, 0, out=boxes)
+    return boxes
+
+
+def random_boxes(num, box_format='tlbr', scale=100):
+    if num:
+        xywh = (torch.rand(num, 4) * scale)
+        if isinstance(scale, int):
+            xywh = xywh.long()
+        if box_format == 'xywh':
+            return xywh
+        elif box_format == 'tlbr':
+            return to_tlbr(xywh)
+        else:
+            raise KeyError(box_format)
+    else:
+        if isinstance(scale, int):
+            return torch.LongTensor()
+        else:
+            return torch.FloatTensor()
