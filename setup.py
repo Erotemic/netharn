d3aeefa018dfb8883e7b5ef31af2f27b43b116ad
joncrall <crallj@rpi.edu>
2018-04-02 00:19:19 -0400

    wip

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ import numpy as np
 from os.path import dirname, join, exists, abspath
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import gpu_setup
 
 
 def parse_version(package):
@@ -130,190 +131,6 @@ def conditional_requirements(pkgmods):
             yield pkgname
 
 
-def find_in_path(name, path):
-    """
-    Find a file in a search path
-    adapted fom
-    http://code.activestate.com/recipes/52224-find-a-file-given-a-search-path/
-    """
-    for dir in path.split(os.pathsep):
-        binpath = join(dir, name)
-        if os.path.exists(binpath):
-            return os.path.abspath(binpath)
-    return None
-
-
-def locate_cuda():
-    """Locate the CUDA environment on the system
-
-    Returns a dict with keys 'home', 'nvcc', 'include', and 'lib64'
-    and values giving the absolute path to each directory.
-
-    Starts by looking for the CUDAHOME env variable. If not found, everything
-    is based on finding 'nvcc' in the PATH.
-    """
-
-    # there doesnt seem to be an accepted standard for the CUDA envvar yet
-    cuda_environs = ['CUDA_HOME', 'CUDA_PATH', 'CUDA_SDK_ROOT_DIR', 'CUDAHOME']
-    cuda_environs = [key for key in cuda_environs if key in os.environ]
-
-    # first check for the env variable CUDA_HOME / CUDA_ROOT / etc.
-    if cuda_environs:
-        cuda_environ = cuda_environs[0]
-        home = os.environ[cuda_environ]
-        nvcc = join(home, 'bin', 'nvcc')
-        if not exists(nvcc):
-            raise EnvironmentError(
-                'The nvcc binary={} does not exist in ${}'.format(
-                    nvcc, cuda_environ))
-    else:
-        # otherwise, search the PATH for NVCC
-        default_path = join(os.sep, 'usr', 'local', 'cuda', 'bin')
-        nvcc = find_in_path('nvcc',
-                            os.environ['PATH'] + os.pathsep + default_path)
-        if nvcc is None:
-            raise EnvironmentError('The nvcc binary could not be '
-                                   'located in your $PATH. '
-                                   'Either add it to your path, '
-                                   'or set $CUDAHOME')
-        home = os.path.dirname(os.path.dirname(nvcc))
-
-    cudaconfig = {'home': home, 'nvcc': nvcc,
-                  'include': join(home, 'include'),
-                  'lib64': join(home, 'lib64')}
-    for k, v in cudaconfig.items():
-        if not os.path.exists(v):
-            raise EnvironmentError('The CUDA %s path could not '
-                                   'be located in %s' % (k, v))
-
-    return cudaconfig
-
-
-CUDACONFIG = locate_cuda()
-
-# Obtain the numpy include directory.  This logic works across numpy versions.
-try:
-    numpy_include = np.get_include()
-except AttributeError:
-    numpy_include = np.get_numpy_include()
-
-
-def customize_compiler_for_nvcc(self):
-    """inject deep into distutils to customize how the dispatch
-    to gcc/nvcc works.
-
-    If you subclass UnixCCompiler, it's not trivial to get your subclass
-    injected in, and still have the right customizations (i.e.
-    distutils.sysconfig.customize_compiler) run on it. So instead of going
-    the OO route, I have this. Note, it's kindof like a wierd functional
-    subclassing going on."""
-
-    # tell the compiler it can processes .cu
-    self.src_extensions.append('.cu')
-
-    # save references to the default compiler_so and _comple methods
-    default_compiler_so = self.compiler_so
-    super = self._compile
-
-    # now redefine the _compile method. This gets executed for each
-    # object but distutils doesn't have the ability to change compilers
-    # based on source extension: we add it.
-    def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
-        print(extra_postargs)
-        if os.path.splitext(src)[1] == '.cu':
-            # use the cuda for .cu files
-            self.set_executable('compiler_so', CUDACONFIG['nvcc'])
-            # use only a subset of the extra_postargs, which are 1-1 translated
-            # from the extra_compile_args in the Extension class
-            postargs = extra_postargs['nvcc']
-        else:
-            postargs = extra_postargs['gcc']
-
-        super(obj, src, ext, cc_args, postargs, pp_opts)
-        # reset the default compiler_so, which we might have changed for cuda
-        self.compiler_so = default_compiler_so
-
-    # inject our redefined _compile method into the class
-    self._compile = _compile
-
-
-# run the customize_compiler
-class custom_build_ext(build_ext):
-    def build_extensions(self):
-        customize_compiler_for_nvcc(self.compiler)
-        build_ext.build_extensions(self)
-
-
-# --------------------------------------------------------
-# Some code was derived from Fast R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick
-# --------------------------------------------------------
-ext_modules = []
-
-
-# yolo_util_m = 'netharn.models.yolo2.utils.'
-# yolo_util_p = yolo_util_m.replace('.', os.path.sep)
-
-# ext_modules += [
-#     Extension(
-#         yolo_util_m + "cython_bbox",
-#         [join(yolo_util_p, "cython_bbox.pyx")],
-#         extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
-#         include_dirs=[numpy_include]
-#     ),
-#     Extension(
-#         yolo_util_m + "cython_yolo",
-#         [join(yolo_util_p, "cython_yolo.pyx")],
-#         extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
-#         include_dirs=[numpy_include]
-#     ),
-#     Extension(
-#         yolo_util_m + "nms.cpu_nms",
-#         [join(yolo_util_p, "nms/cpu_nms.pyx")],
-#         extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
-#         include_dirs=[numpy_include]
-#     ),
-# ]
-
-# ext_modules += [
-#     Extension(
-#         yolo_util_m + 'pycocotools._mask',
-#         sources=[
-#             join(yolo_util_p, 'pycocotools/maskApi.c'),
-#             join(yolo_util_p, 'pycocotools/_mask.pyx'),
-#         ],
-#         include_dirs=[numpy_include, join(yolo_util_p, 'pycocotools')],
-#         extra_compile_args={
-#             'gcc': ['-Wno-cpp', '-Wno-unused-function', '-std=c99']},
-#     ),
-# ]
-
-# ext_modules += [
-#     Extension(yolo_util_m + 'nms.gpu_nms',
-#               [join(yolo_util_p, 'nms/nms_kernel.cu'),
-#                join(yolo_util_p, 'nms/gpu_nms.pyx')],
-#               library_dirs=[CUDACONFIG['lib64']],
-#               libraries=['cudart'],
-#               language='c++',
-#               runtime_library_dirs=[CUDACONFIG['lib64']],
-#               # this syntax is specific to this build system
-#               # we're only going to use certain compiler args with
-#               # nvcc and not with gcc
-#               # the implementation of this trick is in
-#               # customize_compiler() below
-#               extra_compile_args={'gcc': ["-Wno-unused-function"],
-#                                   'nvcc': ['-arch=sm_35',
-#                                            '--ptxas-options=-v',
-#                                            '-c',
-#                                            '--compiler-options',
-#                                            "'-fPIC'"]},
-#               include_dirs=[numpy_include, CUDACONFIG['include']]
-#               ),
-# ]
-
-
 def clean():
     """
     __file__ = ub.truepath('~/code/netharn/setup.py')
@@ -375,81 +192,93 @@ def clean():
         ub.delete(dpath, verbose=1)
 
 
-# class TorchExtension(object):
-#     """
-#     customized hacked extension
+CUDACONFIG = locate_cuda()
 
-#     TODO: is there a better way to do this?
-#     """
+# Obtain the numpy include directory.  This logic works across numpy versions.
+try:
+    numpy_include = np.get_include()
+except AttributeError:
+    numpy_include = np.get_numpy_include()
 
-#     def __init__(self, name, sources, cuda_sources, extra_compile_args={},
-#                  with_cuda=True):
-#         self.name = name
-#         self.sources = sources
-#         self.cuda_sources = cuda_sources
-#         self.extra_compile_args = extra_compile_args
-#         self.with_cuda = with_cuda
 
-#     def build(self):
-#         from torch.utils.ffi import create_extension
-#         import ubelt as ub
+# run the customize_compiler
+class custom_build_ext(build_ext):
+    def build_extensions(self):
+        gpu_setup.customize_compiler_for_nvcc(self.compiler)
+        build_ext.build_extensions(self)
 
-#         sources = [p for p in self.sources if p.endswith('.c')]
-#         headers = [p for p in self.sources if p.endswith('.h')]
-#         cu_sources = [p for p in self.cuda_sources if p.endswith('.cu')]
 
-#         extra_objects = []
-#         defines = []
-#         if self.with_cuda:
-#             sources += [p for p in self.cuda_sources if p.endswith('.c')]
-#             headers += [p for p in self.cuda_sources if p.endswith('.h')]
-#             cu_objects = [p + '.o' for p in cu_sources]
+# --------------------------------------------------------
+# Some code was derived from Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+ext_modules = []
 
-#             extra = ' '.join(self.extra_compile_args.get('nvcc', []))
-#             command_fmt = '{nvcc_exe} -c -o {cu_objects} {cu_sources} {extra}'
-#             command = command_fmt.format(
-#                 nvcc_exe=CUDACONFIG['nvcc'],
-#                 cu_objects=' '.join(cu_objects),
-#                 cu_sources=' '.join(cu_sources),
-#                 extra=extra,
-#             )
-#             info = ub.cmd(command, verbout=1, verbose=2)
-#             if info['ret'] != 0:
-#                 raise Exception('Failed to build extension ' + self.name)
 
-#             for fpath in cu_objects:
-#                 if not exists(fpath):
-#                     raise Exception('Object {} does not exist'.format(fpath))
+util_m = 'netharn.utils.'
+util_p = util_m.replace('.', os.path.sep)
 
-#             extra_objects += [abspath(p) for p in cu_objects]
-#             defines += [('WITH_CUDA', None)]
+ext_modules += [
+#     Extension(
+#         util_m + "cython_bbox",
+#         [join(util_p, "cython_bbox.pyx")],
+#         extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
+#         include_dirs=[numpy_include]
+#     ),
+#     Extension(
+#         util_m + "cython_yolo",
+#         [join(util_p, "cython_yolo.pyx")],
+#         extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
+#         include_dirs=[numpy_include]
+#     ),
+    Extension(
+        util_m + "nms.cpu_nms",
+        [join(util_p, "nms/cpu_nms.pyx")],
+        extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
+        include_dirs=[numpy_include]
+    ),
+]
 
-#         ffi = create_extension(
-#             self.name,
-#             headers=headers,
-#             sources=sources,
-#             define_macros=defines,
-#             relative_to=__file__,
-#             with_cuda=self.with_cuda,
-#             extra_objects=extra_objects,
-#             # extra_compile_args=self.extra_compile_args
-#         )
-#         ffi.build()
 
-# yolo_layers_m = 'netharn.models.yolo2.layers.'
-# yolo_layers_p = yolo_layers_m.replace('.', os.path.sep)
+ext_modules += [
+    Extension(util_m + 'nms.gpu_nms',
+              [join(util_p, 'nms/nms_kernel.cu'),
+               join(util_p, 'nms/gpu_nms.pyx')],
+              library_dirs=[CUDACONFIG['lib64']],
+              libraries=['cudart'],
+              language='c++',
+              runtime_library_dirs=[CUDACONFIG['lib64']],
+              # this syntax is specific to this build system
+              # we're only going to use certain compiler args with
+              # nvcc and not with gcc
+              # the implementation of this trick is in
+              # customize_compiler() below
+              extra_compile_args={'gcc': ["-Wno-unused-function"],
+                                  'nvcc': ['-arch=sm_35',
+                                           '--ptxas-options=-v',
+                                           '-c',
+                                           '--compiler-options',
+                                           "'-fPIC'"]},
+              include_dirs=[numpy_include, CUDACONFIG['include']]
+              ),
+]
+
+# layers_m = 'netharn.layers.'
+# layers_p = layers_m.replace('.', os.path.sep)
 
 # # Is there a better way to incorporate these into normal ext_modules
 # reorg_ext = TorchExtension(
-#     yolo_layers_m + 'reorg._ext.reorg_layer',
+#     layers_m + 'reorg._ext.reorg_layer',
 #     sources=[
-#         join(yolo_layers_p, 'reorg/src/reorg_cpu.c'),
-#         join(yolo_layers_p, 'reorg/src/reorg_cpu.h')
+#         join(layers_p, 'reorg/src/reorg_cpu.c'),
+#         join(layers_p, 'reorg/src/reorg_cpu.h')
 #     ],
 #     cuda_sources=[
-#         join(yolo_layers_p, 'reorg/src/reorg_cuda_kernel.cu'),
-#         join(yolo_layers_p, 'reorg/src/reorg_cuda.c'),
-#         join(yolo_layers_p, 'reorg/src/reorg_cuda.h')
+#         join(layers_p, 'reorg/src/reorg_cuda_kernel.cu'),
+#         join(layers_p, 'reorg/src/reorg_cuda.c'),
+#         join(layers_p, 'reorg/src/reorg_cuda.h')
 #     ],
 #     extra_compile_args={
 #         'gcc': ["-Wno-unused-function"],
@@ -458,8 +287,8 @@ def clean():
 # )
 
 # reorg_ext.build()
-# from netharn.models.yolo2.layers.reorg import reorg_layer
-# from netharn.models.yolo2.layers.reorg._ext.reorg_layer import _reorg_layer
+# from netharn.layers.reorg import reorg_layer
+# from netharn.layers.reorg._ext.reorg_layer import _reorg_layer
 # print('reorg_layer = {!r}'.format(dir(reorg_layer)))
 # print('reorg_layer.reorg_layer = {!r}'.format(dir(reorg_layer.reorg_layer)))
 # import ubelt as ub
@@ -467,15 +296,15 @@ def clean():
 # sys.exit(0)
 
 # roi_ext = TorchExtension(
-#     yolo_layers_m + 'roi_pooling._ext.roi_pooling',
+#     layers_m + 'roi_pooling._ext.roi_pooling',
 #     sources=[
-#         join(yolo_layers_p, 'roi_pooling/src/roi_pooling.c'),
-#         join(yolo_layers_p, 'roi_pooling/src/roi_pooling.h')
+#         join(layers_p, 'roi_pooling/src/roi_pooling.c'),
+#         join(layers_p, 'roi_pooling/src/roi_pooling.h')
 #     ],
 #     cuda_sources=[
-#         join(yolo_layers_p, 'roi_pooling/src/cuda/roi_pooling_kernel.cu'),
-#         join(yolo_layers_p, 'roi_pooling/src/roi_pooling_cuda.c'),
-#         join(yolo_layers_p, 'roi_pooling/src/roi_pooling_cuda.h')
+#         join(layers_p, 'roi_pooling/src/cuda/roi_pooling_kernel.cu'),
+#         join(layers_p, 'roi_pooling/src/roi_pooling_cuda.c'),
+#         join(layers_p, 'roi_pooling/src/roi_pooling_cuda.h')
 #     ],
 #     extra_compile_args={
 #         'gcc': ["-Wno-unused-function"],
@@ -520,8 +349,8 @@ if __name__ == '__main__':
         packages=find_packages(),
 
         # inject our custom nvcc trigger
-        # cmdclass={'build_ext': custom_build_ext},
-        # ext_modules=ext_modules,
+        cmdclass={'build_ext': custom_build_ext},
+        ext_modules=ext_modules,
 
         classifiers=[
             # List of classifiers available at:
