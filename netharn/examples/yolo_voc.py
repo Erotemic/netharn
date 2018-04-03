a8393058bbb8287cc709d3798e71fee17568ebfa
Jon Crall <jon.crall@kitware.com>
2018-04-03 11:54:56 -0400

    wip

--- netharn/examples/yolo_voc.py
+++ netharn/examples/yolo_voc.py
@@ -250,6 +250,7 @@ class YoloVOCDataset(nh.data.voc.VOCDataset):
 
         return chw01, label
 
+    @ub.memoize_method
     def _load_image(self, index):
         return super(YoloVOCDataset, self)._load_image(index)
 
