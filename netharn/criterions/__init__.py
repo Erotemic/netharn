e5aa153965d3b70b82bd28aa0b977d083b839e2e
joncrall <jon.crall@kitware.com>
2018-04-20 11:28:03 -0400

    Toggled dynamic off

--- netharn/criterions/__init__.py
+++ netharn/criterions/__init__.py
@@ -4,7 +4,7 @@ python -c "import ubelt._internal as a; a.autogen_init('netharn.criterions')"
 # flake8: noqa
 from torch.nn.modules.loss import CrossEntropyLoss, MSELoss, TripletMarginLoss
 
-__DYNAMIC__ = True
+__DYNAMIC__ = False
 if __DYNAMIC__:
     from ubelt._internal import dynamic_import
     exec(dynamic_import(__name__))
