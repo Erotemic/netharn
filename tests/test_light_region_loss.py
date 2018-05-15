b4612bb0ebaeeed0da9947d844c678dee876b9d5
joncrall <jon.crall@kitware.com>
2018-05-15 15:40:52 -0400

    wip

--- tests/test_light_region_loss.py
+++ tests/test_light_region_loss.py
@@ -1,14 +1,14 @@
 import torch
-from netharn.models.yolo2.light_region_loss import RegionLoss
-from netharn.models.yolo2.light_yolo import Yolo
-from brambox.boxes.annotations import Annotation
-import numpy as np
+# from netharn.models.yolo2.light_region_loss import RegionLoss
+# from netharn.models.yolo2.light_yolo import Yolo
+# import numpy as np
 
 
 def as_anno(class_id, x_center, y_center, w, h, Win, Hin):
     """
     Construct an BramBox annotation using the basic YOLO box format
     """
+    from brambox.boxes.annotations import Annotation
     anno = Annotation()
     anno.class_id = class_id
     anno.x_top_left = (x_center - w / 2) * Win
@@ -44,70 +44,69 @@ def demodata_targets(input_size):
     return target_brambox, target_tensor
 
 
-def test_tensor_brambox_compat_ground_truth_encoding():
-    """
-    Check that brambox and tensor boxes produce the same ground truth encoding
-    """
+# def test_tensor_brambox_compat_ground_truth_encoding():
+#     """
+#     Check that brambox and tensor boxes produce the same ground truth encoding
+#     """
 
-    network = Yolo(num_classes=2, conf_thresh=4e-2)
-    self = RegionLoss(num_classes=network.num_classes, anchors=network.anchors)
+#     network = Yolo(num_classes=2, conf_thresh=4e-2)
+#     self = RegionLoss(num_classes=network.num_classes, anchors=network.anchors)
 
-    Win, Hin = 96, 96
-    target_brambox, target_tensor = demodata_targets(input_size=(Win, Hin))
+#     Win, Hin = 96, 96
+#     target_brambox, target_tensor = demodata_targets(input_size=(Win, Hin))
 
-    # convert network input size (e.g. 416x416) to output size (e.g. 13x13)
-    nW, nH = (Win // self.reduction, Hin // self.reduction)
+#     # convert network input size (e.g. 416x416) to output size (e.g. 13x13)
+#     nW, nH = (Win // self.reduction, Hin // self.reduction)
 
-    pred_boxes = torch.rand(90, 4)
-    pred_confs = torch.rand(90)
+#     pred_boxes = torch.rand(90, 4)
 
-    _ret_ten = self._build_targets_tensor(pred_boxes, pred_confs, target_tensor, nH, nW)
-    _ret_box = self._build_targets_brambox(pred_boxes, pred_confs, target_brambox, nH, nW)
+#     _ret_ten = self._build_targets_tensor(pred_boxes, target_tensor, nH, nW)
+#     # _ret_box = self._build_targets_brambox(pred_boxes, target_brambox, nH, nW)
 
-    keys = ['coord_mask', 'conf_mask', 'cls_mask', 'tcoord', 'tconf', 'tcls']
+#     keys = ['coord_mask', 'conf_mask', 'cls_mask', 'tcoord', 'tconf', 'tcls']
 
-    errors = []
-    for key, item1, item2 in zip(keys, _ret_box, _ret_ten):
-        bad_flags = ~np.isclose(item1, item2)
-        if np.any(bad_flags):
-            bad_idxs = np.where(bad_flags)
-            msg = 'key={} did not agree at indices {}. values1={} values2={}'.format(
-                key, bad_idxs, item1[bad_flags], item2[bad_flags])
-            errors.append(msg)
+#     errors = []
+#     for key, item1, item2 in zip(keys, _ret_box, _ret_ten):
+#         bad_flags = ~np.isclose(item1, item2)
+#         if np.any(bad_flags):
+#             bad_idxs = np.where(bad_flags)
+#             msg = 'key={} did not agree at indices {}. values1={} values2={}'.format(
+#                 key, bad_idxs, item1[bad_flags], item2[bad_flags])
+#             errors.append(msg)
 
-    if errors:
-        raise AssertionError('\n---\n'.join(errors))
+#     if errors:
+#         raise AssertionError('\n---\n'.join(errors))
 
 
-def test_tensor_brambox_compat_ground_truth_loss():
-    """
-    Check that brambox and tensor boxes produce the same ground truth encoding
-    """
-    network = Yolo(num_classes=2, conf_thresh=4e-2)
-    self = RegionLoss(num_classes=network.num_classes, anchors=network.anchors)
+# def test_tensor_brambox_compat_ground_truth_loss():
+#     """
+#     Check that brambox and tensor boxes produce the same ground truth encoding
+#     """
+#     network = Yolo(num_classes=2, conf_thresh=4e-2)
+#     self = RegionLoss(num_classes=network.num_classes, anchors=network.anchors)
 
-    # Win, Hin = 96, 96
-    Win, Hin = 416, 416
-    target_brambox, target_tensor = demodata_targets(input_size=(Win, Hin))
+#     # Win, Hin = 96, 96
+#     Win, Hin = 416, 416
+#     target_brambox, target_tensor = demodata_targets(input_size=(Win, Hin))
 
-    # construct dummy data and run it through the network
-    im_data = torch.randn(len(target_brambox), 3, Hin, Win)
-    output = network.forward(im_data)
-    print('')
+#     # construct dummy data and run it through the network
+#     im_data = torch.randn(len(target_brambox), 3, Hin, Win)
+#     output = network.forward(im_data)
+#     print('')
 
-    # Brambox and Tensor formats should produce the same loss
-    loss_brambox = self(output, target_brambox).data.cpu().numpy()
-    loss_tensor = self(output, target_tensor).data.cpu().numpy()
-    print('loss_brambox = {!r}'.format(loss_brambox))
-    print('loss_tensor = {!r}'.format(loss_tensor))
-    assert np.all(np.isclose(loss_brambox, loss_tensor))
+#     # Brambox and Tensor formats should produce the same loss
+#     loss_brambox = self(output, target_brambox).data.cpu().numpy()
+#     loss_tensor = self(output, target_tensor).data.cpu().numpy()
+#     print('loss_brambox = {!r}'.format(loss_brambox))
+#     print('loss_tensor = {!r}'.format(loss_tensor))
+#     assert np.all(np.isclose(loss_brambox, loss_tensor))
 
-    # Brambox and Tensor formats should produce the same loss
-    loss_brambox = self(output, target_brambox, seen=9999).data.cpu().numpy()
-    loss_tensor = self(output, target_tensor, seen=9999).data.cpu().numpy()
-    print('loss_brambox = {!r}'.format(loss_brambox))
-    print('loss_tensor = {!r}'.format(loss_tensor))
-    assert np.all(np.isclose(loss_brambox, loss_tensor))
+#     # Brambox and Tensor formats should produce the same loss
+#     loss_brambox = self(output, target_brambox, seen=9999).data.cpu().numpy()
+#     loss_tensor = self(output, target_tensor, seen=9999).data.cpu().numpy()
+#     print('loss_brambox = {!r}'.format(loss_brambox))
+#     print('loss_tensor = {!r}'.format(loss_tensor))
+#     assert np.all(np.isclose(loss_brambox, loss_tensor))
 
 
 if __name__ == '__main__':
