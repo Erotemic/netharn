13b84362f0e50b95057567e4a15f1e602d4101cd
joncrall <erotemic@gmail.com>
2018-06-03 20:02:45 -0400

    wip

--- netharn/util/util_cachestamp.py
+++ netharn/util/util_cachestamp.py
@@ -80,9 +80,10 @@ class CacheStamp(object):
         """
         Get the hash of the each product file
         """
+        import xxhash  # much faster than builtin hashers
         products = self._rectify_products(product)
-        # product_file_hash = [ub.hash_file(p) for p in products]
-        product_file_hash = [hash_file2(p) for p in products]
+        product_file_hash = [ub.hash_file(p, hasher=xxhash.xxh64)
+                             for p in products]
         return product_file_hash
 
     def expired(self, cfgstr=None, product=None):
@@ -134,45 +135,6 @@ class CacheStamp(object):
                     'The stamped product must exist: {}'.format(products))
             certificate['product_file_hash'] = self._product_file_hash(products)
         self.cacher.save(certificate, cfgstr=cfgstr)
-
-
-def hash_file2(fpath, blocksize=65536, hasher='xx64'):
-    r"""
-    Hashes the data in a file on disk using xxHash
-
-    xxHash is much faster than sha1, bringing computation time down from .57
-    seconds to .12 seconds for a 387M file.
-
-    xdata = 2 ** np.array([8, 12, 14, 16])
-    ydatas = ub.ddict(list)
-    for blocksize in xdata:
-        print('blocksize = {!r}'.format(blocksize))
-        ydatas['sha1'].append(ub.Timerit(2).call(ub.hash_file, my_weights_fpath_, hasher='sha1', blocksize=blocksize).min())
-        ydatas['sha256'].append(ub.Timerit(2).call(ub.hash_file, my_weights_fpath_, hasher='sha256', blocksize=blocksize).min())
-        ydatas['sha512'].append(ub.Timerit(2).call(ub.hash_file, my_weights_fpath_, hasher='sha512', blocksize=blocksize).min())
-        ydatas['md5'].append(ub.Timerit(2).call(ub.hash_file, my_weights_fpath_, hasher='md5', blocksize=blocksize).min())
-        ydatas['xx32'].append(ub.Timerit(2).call(hash_file2, my_weights_fpath_, hasher='xx32', blocksize=blocksize).min())
-        ydatas['xx64'].append(ub.Timerit(2).call(hash_file2, my_weights_fpath_, hasher='xx64', blocksize=blocksize).min())
-
-    nh.util.qtensure()
-    nh.util.multi_plot(xdata, ydatas)
-    """
-    import xxhash
-    if hasher == 'xx32':
-        hasher = xxhash.xxh32()
-    elif hasher == 'xx64':
-        hasher = xxhash.xxh64()
-
-    with open(fpath, 'rb') as file:
-        buf = file.read(blocksize)
-        # otherwise hash the entire file
-        while len(buf) > 0:
-            hasher.update(buf)
-            buf = file.read(blocksize)
-    # Get the hashed representation
-    text = ub.util_hash._digest_hasher(hasher, hashlen=None,
-                                       base=ub.util_hash.DEFAULT_ALPHABET)
-    return text
 
 
 if __name__ == '__main__':
