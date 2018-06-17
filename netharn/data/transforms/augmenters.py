Fixed mAP computation

netharn/data/transforms/augmenters.py
@@ -307,17 +307,19 @@ class Resize(augmenter_base.ParamatarizedAugmenter):
 
         """
         shift, scale, embed_size = self._letterbox_transform(orig_size, target_size)
-        new_boxes = boxes.scale(scale).shift(shift)
+        new_boxes = boxes.scale(scale).translate(shift)
         return new_boxes
 
     def _boxes_letterbox_invert(self, boxes, orig_size, target_size):
         """
-        Undo the letterbox transform for these bounding boxes
+        Undo the letterbox transform for these bounding boxes. Moves
+        the box from `target_size` coordinatse (which are probably square)
+        to `orig_size` coordinates (which are probably nto square).
 
         Args:
-            boxes (util.Boxes) : boxes to rework
+            boxes (util.Boxes) : boxes to rework in `target_size` coordinates
             orig_size : original wh of the image
-            target_size : network input wh
+            target_size : network input wh (i.e. inp_size)
 
         Example:
             >>> target_size = (416, 416)
@@ -329,7 +331,7 @@ class Resize(augmenter_base.ParamatarizedAugmenter):
             >>> assert np.allclose(cxywh_norm2.data, cxywh_norm.data)
         """
         shift, scale, embed_size = self._letterbox_transform(orig_size, target_size)
-        new_boxes = boxes.shift(-shift).scale(1.0 / scale)
+        new_boxes = boxes.translate(-shift).scale(1.0 / scale)
         return new_boxes
 
     def _letterbox_transform(self, orig_size, target_size):
@@ -339,8 +341,14 @@ class Resize(augmenter_base.ParamatarizedAugmenter):
         scale should be applied before the shift.
 
         Args:
-            inp_size : network input wh
             orig_size : original wh of the image
+            target_size : network input wh
+
+        Example:
+            >>> Resize(None)._letterbox_transform([5, 10], [10, 10])
+            (array([2, 0]), array([1., 1.]), array([ 5, 10]))
+            >>> Resize(None)._letterbox_transform([10, 5], [10, 10])
+            (array([0, 2]), array([1., 1.]), array([10,  5]))
         """
         # determine if width or the height should be used as the scale factor.
         orig_size = np.array(orig_size)
@@ -350,7 +358,9 @@ class Resize(augmenter_base.ParamatarizedAugmenter):
 
         # Whats the closest integer size we can resize to?
         embed_size = np.round(orig_size * sf).astype(np.int)
-        # Determine how much padding we need for the top left side
+        # Determine how much padding we need for the top/left side
+        # Note: the right/bottom side might need an extra pixel of padding
+        # depending on rounding issues.
         shift = np.round((target_size - embed_size) / 2).astype(np.int)
 
         scale = embed_size / orig_size

netharn/examples/yolo_voc.py
@@ -62,19 +62,6 @@ class YoloVOCDataset(nh.data.voc.VOCDataset):
             self.base_wh + (self.factor * i) for i in range(*scales)])
         self.multi_scale_out_size = self.multi_scale_inp_size // self.factor
 
-        # Original YOLO Anchors
-        if ub.argflag('--orig'):
-            self.anchors = np.asarray([(1.08, 1.19), (3.42, 4.41),
-                                       (6.63, 11.38), (9.42, 5.11),
-                                       (16.62, 10.52)],
-                                      dtype=np.float)
-        else:
-            # Lightnet Anchors
-            self.anchors = np.array([(1.3221, 1.73145), (3.19275, 4.00944),
-                                     (5.05587, 8.09892), (9.47112, 4.84053),
-                                     (11.2364, 10.0071)])
-
-        self.num_anchors = len(self.anchors)
         self.augmenter = None
 
         if 'train' in split:
@@ -492,7 +479,6 @@ class YoloHarn(nh.FitHarn):
 
         bsize = len(targets)
         for bx in range(bsize):
-            postitem = asnumpy(postout[bx])
             target = asnumpy(targets[bx]).reshape(-1, 5)
             true_cxywh = target[:, 1:5]
             true_cxs = target[:, 0]
@@ -511,6 +497,7 @@ class YoloHarn(nh.FitHarn):
             bg_weight = float(asnumpy(bg_weights[bx]))
 
             # Unpack postprocessed predictions
+            postitem = asnumpy(postout[bx])
             sboxes = postitem.reshape(-1, 6)
             pred_cxywh = sboxes[:, 0:4]
             pred_scores = sboxes[:, 4]
@@ -839,6 +826,18 @@ def setup_harness(bsize=16, workers=0):
             90: lr * 0.01 / simulated_bsize,
         }
 
+    # Original YOLO Anchors
+    if ub.argflag('--orig'):
+        anchors = np.asarray([(1.08, 1.19), (3.42, 4.41),
+                              (6.63, 11.38), (9.42, 5.11),
+                              (16.62, 10.52)],
+                             dtype=np.float)
+    else:
+        # Lightnet Anchors
+        anchors = np.array([(1.3221, 1.73145), (3.19275, 4.00944),
+                            (5.05587, 8.09892), (9.47112, 4.84053),
+                            (11.2364, 10.0071)])
+
     hyper = nh.HyperParams(**{
         'nice': nice,
         'workdir': ub.truepath('~/work/voc_yolo2'),
@@ -854,7 +853,7 @@ def setup_harness(bsize=16, workers=0):
 
         'model': (light_yolo.Yolo, {
             'num_classes': datasets['train'].num_classes,
-            'anchors': datasets['train'].anchors,
+            'anchors': anchors,
             'conf_thresh': 0.001,
             # nms_thresh=0.5 to reproduce original yolo
             # nms_thresh=0.4 to reproduce lightnet
@@ -863,7 +862,7 @@ def setup_harness(bsize=16, workers=0):
 
         'criterion': (light_region_loss.RegionLoss, {
             'num_classes': datasets['train'].num_classes,
-            'anchors': datasets['train'].anchors,
+            'anchors': anchors,
             'object_scale': 5.0,
             'noobject_scale': 1.0,
             'class_scale': 1.0,

netharn/metrics/detections.py
@@ -301,11 +301,21 @@ def _ave_precision(rec, prec, use_07_metric=False):
 
         # and sum (\Delta recall) * prec
         ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+
+    if False:
+        # sklearn metric
+        ap = -np.sum(np.diff(rec[::-1]) * np.array(prec[::-1])[:-1])
     return ap
 
 
-def ave_precisions(y, labels=None, use_07_metric=False):
+def ave_precisions(y, labels=None, method='voc2007', use_07_metric=None):
     """
+
+    Args:
+        y (pd.DataFrame): pre-measured frames of predictions, truth,
+            weight and class.
+        method (str): either voc2007 or sklearn
+
     Example:
         >>> y_true = [1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4,-1,-1,-1]
         >>> y_pred = [1, 3,-1, 2,-1, 1, 2, 1,-1, 3, 3, 3, 3, 3, 4, 4, 3, 1, 1]
@@ -337,33 +347,9 @@ def ave_precisions(y, labels=None, use_07_metric=False):
         >>> print('mAP = {:.4f}'.format(mAP))
         mAP = 0.5875
     """
-    def group_metrics(group):
-        # compute metrics on a per class basis
-        if group is None:
-            return np.nan
-        group = group.sort_values('score', ascending=False)
-        npos = sum(group.true >= 0)
-        dets = group[group.pred > -1]
-        if npos == 0:
-            return np.nan
-        if len(dets) == 0:
-            if npos == 0:
-                return np.nan
-            return 0.0
-        tp = (dets.pred == dets.true).values.astype(np.uint8)
-        fp = 1 - tp
-        fp = np.cumsum(fp)
-        tp = np.cumsum(tp)
-
-        eps = np.finfo(np.float64).eps
-        if npos == 0:
-            rec = 1
-        else:
-            rec = tp / npos
-        prec = tp / np.maximum(tp + fp, eps)
-
-        ap = _ave_precision(rec, prec, use_07_metric=use_07_metric)
-        return ap
+    method = 'sklearn'
+    if use_07_metric is True:
+        method = 'voc2007'
 
     if 'cx' not in y:
         cx = y['true'].copy()
@@ -381,11 +367,92 @@ def ave_precisions(y, labels=None, use_07_metric=False):
     for cx in labels:
         # for cx, group in cx_to_group.items():
         group = cx_to_group.get(cx, None)
-        ap = group_metrics(group)
+        ap = _group_metrics(group, method)
         class_aps.append((cx, ap))
 
     ave_precs = pd.DataFrame(class_aps, columns=['cx', 'ap'])
     return ave_precs
+
+
+def _group_metrics(group, method):
+    # compute metrics on a per class basis
+    if group is None:
+        return np.nan
+
+    # References [Manning2008] and [Everingham2010] present alternative
+    # variants of AP that interpolate the precision-recall curve. Currently,
+    # average_precision_score does not implement any interpolated variant
+    # http://scikit-learn.org/stable/modules/model_evaluation.html
+
+    # g2 = group[group.weight > 0]
+    # prec2, rec2, thresh = sklearn.metrics.precision_recall_curve(
+    #     (g2.true > -1).values,
+    #     g2.score.values,
+    #     sample_weight=g2.weight.values)
+    # ap2 = _ave_precision(rec2, prec2, use_07_metric=use_07_metric)
+    # print('ap2 = {!r}'.format(ap2))
+
+    if method == 'sklearn':
+        # In the future, we should simply use the sklearn version
+        # which gives nice easy to reproduce results.
+        import sklearn.metrics
+        df = group
+        ap = sklearn.metrics.average_precision_score(
+            y_true=(df['true'].values == df['pred'].values).astype(np.int),
+            y_score=df['score'].values,
+            sample_weight=df['weight'].values,
+        )
+        return ap
+
+    if False and method == 'voc2007':
+        import sklearn.metrics
+
+        # The VOC scoring is weird, and does not conform to sklearn We
+        # overcount the number of trues and use unweighted PR curves instead of
+        # simply using the weighted variant (that albiet gives lower scores)
+        npos = group[group.true >= 0].weight.sum()
+        dets = group[group.pred > -1]
+
+        fps, tps, thresholds = sklearn.metrics.ranking._binary_clf_curve(
+            (dets.pred == dets.true).values, dets.score.values,
+            sample_weight=None)
+
+        precision = tps / (tps + fps)
+        recall = tps / npos
+
+        last_ind = tps.searchsorted(tps[-1])
+        sl = slice(last_ind, None, -1)
+        prec3 = np.r_[precision[sl], 1]
+        rec3 = np.r_[recall[sl], 0]
+        # thre3 = thresholds[sl]
+
+        ap = _ave_precision(rec3, prec3, use_07_metric=method == 'voc2007')
+        return ap
+
+    if method == 'voc2007':
+        group = group.sort_values('score', ascending=False)
+        # npos = sum(group.true >= 0)
+        npos = group[group.true >= 0].weight.sum()
+        dets = group[group.pred > -1]
+        if npos > 0 and len(dets) > 0:
+            tp = (dets.pred == dets.true).values.astype(np.int)
+            fp = 1 - tp
+            fp_cum = np.cumsum(fp)
+            tp_cum = np.cumsum(tp)
+
+            eps = np.finfo(np.float64).eps
+            rec = 1 if npos == 0 else tp_cum / npos
+            prec = tp_cum / np.maximum(tp_cum + fp_cum, eps)
+
+            ap = _ave_precision(rec, prec, use_07_metric=(method == 'voc2007'))
+            return ap
+        else:
+            if npos == 0:
+                return np.nan
+            if len(dets) == 0:
+                if npos == 0:
+                    return np.nan
+                return 0.0
 
 
 if __name__ == '__main__':