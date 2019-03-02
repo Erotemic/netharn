4a7c80c09944ca04a7b1ba9ddbadc91b265d1353
Jon Crall <jon.crall@kitware.com>
2019-03-02 12:56:35 -0500

    Change patch version

--- netharn/__init__.py
+++ netharn/__init__.py
@@ -3,7 +3,7 @@
 # (Remote the --dry to actually run it)
 mkinit netharn --noattrs --dry
 """
-__version__ = '0.1.5.dev0'
+__version__ = '0.1.5.1.dev0'
 
 from netharn.device import (XPU,)
 from netharn.fit_harn import (FitHarn,)
