1a36a31257585364a817321a3b8f7e27d3c8b1eb
joncrall <erotemic@gmail.com>
2020-02-05 20:15:34 -0500

    Use new gpu_info mode

--- netharn/device.py
+++ netharn/device.py
@@ -688,15 +688,51 @@ def find_unused_gpu(min_memory=0):
     return None
 
 
-def gpu_info(new_mode=False):
+def _query_nvidia_smi(mode, fields):
+    """
+    Runs nvidia smi in query mode
+
+    Args:
+        mode (str): the query cli flag to pass to nvidia-smi
+        fields (List[str]): csv header fields to query
+
+    Returns:
+        List[Dict[str, str]]: parsed csv output
+    """
+    header = ','.join(fields)
+    command = [
+        'nvidia-smi',
+        '--{}={}'.format(mode, header),
+        '--format=csv,noheader'
+    ]
+    info = ub.cmd(command)
+    if info['ret'] != 0:
+        print(info['out'])
+        print(info['err'])
+        raise Exception('unable to call nvidia-smi: ret={}'.format(
+            info['ret']))
+    rows = []
+    for line in info['out'].split('\n'):
+        line = line.strip()
+        if line:
+            parts = [p.strip() for p in line.split(',')]
+            row = ub.dzip(fields, parts)
+            rows.append(row)
+    return rows
+
+
+def gpu_info(new_mode=True):
     """
     Run nvidia-smi and parse output
+
+    Args:
+        new_mode: internal argument that changes the underlying implementation
 
     Returns:
         OrderedDict: info about each GPU indexed by gpu number
 
     Note:
-        Does not gaurentee CUDA is installed.
+        Not gaurenteed to work if CUDA is not installed.
 
     Warnings:
         if nvidia-smi is not installed
@@ -706,15 +742,49 @@ def gpu_info(new_mode=False):
 
     Example:
         >>> # xdoctest: +REQUIRES(--cuda)
-        >>> from netharn.device import *
+        >>> from netharn.device import gpu_info
         >>> gpus = gpu_info()
-        >>> print('gpus = {}'.format(ub.repr2(gpus, nl=3)))
         >>> assert len(gpus) == torch.cuda.device_count()
+        >>> # xdoctest: +IGNORE_WANT
+        >>> print('gpus = {}'.format(ub.repr2(gpus, nl=4)))
+        gpus = {
+            0: {
+                'gpu_uuid': 'GPU-348ebe36-252b-46fa-8a97-477ae331f6f4',
+                'index': '0',
+                'mem_avail': 10013.0,
+                'mem_total': 11170.0,
+                'mem_used': 1157.0,
+                'memory.free': '10013 MiB',
+                'memory.total': '11170 MiB',
+                'memory.used': '1157 MiB',
+                'name': 'GeForce GTX 1080 Ti',
+                'num': 0,
+                'num_compute_procs': 1,
+                'procs': [
+                    {
+                        'gpu_num': 0,
+                        'gpu_uuid': 'GPU-348ebe36-252b-46fa-8a97-477ae331f6f4',
+                        'name': '/usr/bin/python',
+                        'pid': '19912',
+                        'type': 'C',
+                        'used_memory': '567 MiB',
+                    },
+                ],
+            },
+        }
+
+
     """
     pass
 
     """
     Ignore:
+
+        # official nvidia-smi python bindings
+        pip install nvidia-ml-py
+
+        import pynvml
+
         # TODO: make more efficient calls to nvidia-smi
 
         utilization.gpu
@@ -727,6 +797,8 @@ def gpu_info(new_mode=False):
         name
         count
 
+        nvidia-smi pmon --count 1
+
         nvidia-smi  -h
         nvidia-smi  --help-query-compute-apps
         nvidia-smi  --help-query-gpu
@@ -738,39 +810,93 @@ def gpu_info(new_mode=False):
 
         nvidia-smi  --query-gpu="index,memory.total,memory.used,memory.free,count,name,gpu_uuid" --format=csv
         nvidia-smi  --query-compute-apps="pid,name,gpu_uuid,used_memory" --format=csv
+        nvidia-smi  --query-accounted-apps="gpu_name,pid" --format=csv
+
+        import timerit
+        ti = timerit.Timerit(40, bestof=5, verbose=2)
+        for timer in ti.reset('new1'):
+            with timer:
+                gpu_info(True)
+        for timer in ti.reset('old'):
+            with timer:
+                gpu_info(False)
+        for timer in ti.reset('xml'):
+            with timer:
+                gpu_info('xml')
+
+        xdev.profile_now(gpu_info)('xml')
+
+        for timer in ti.reset('cmd'):
+            with timer:
+                ub.cmd(['nvidia-smi', '--query', '--xml-format'])
+
+        for timer in ti.reset('check_output'):
+            with timer:
+                import subprocess
+                subprocess.check_output(['nvidia-smi', '--query', '--xml-format'])
     """
+    if new_mode == 'xml':
+        # Parse info out of the nvidia xml query
+        # note, that even though this has less calls to nvidia-smi, there
+        # is a lot more output, which makes it the slowest method especially
+        # for multi-gpu systems
+        import xml.etree.ElementTree as ET
+
+        info = ub.cmd(['nvidia-smi', '--query', '--xml-format'])
+        if info['ret'] != 0:
+            print(info['out'])
+            print(info['err'])
+            warnings.warn('Problem running nvidia-smi: ret='.format(
+                info['ret']))
+            return None
+        xml_string = info['out']
+        root = ET.fromstring(xml_string)
+
+        gpus = {}
+        for gpu_elem in root.findall('gpu'):
+            gpu = {}
+            gpu['uuid'] = gpu_elem.find('uuid').text
+            gpu['name'] = gpu_elem.find('product_name').text
+            gpu['num'] = int(gpu_elem.find('minor_number').text)
+            gpu['procs'] = [
+                {item.tag: item.text for item in proc_elem}
+                for proc_elem in gpu_elem.find('processes')
+            ]
+
+            for item in gpu_elem.find('fb_memory_usage'):
+                gpu['memory.' + item.tag] = item.text
+
+            gpu['mem_used'] = float(gpu['memory.used'].strip().replace('MiB', ''))
+            gpu['mem_total'] = float(gpu['memory.total'].strip().replace('MiB', ''))
+            gpu['mem_avail'] = gpu['mem_total'] - gpu['mem_used']
+            gpus[gpu['num']] = gpu
+
+            # Let each GPU know how many processes are currently using it
+            num_compute_procs = 0
+            num_graphics_procs = 0
+            for proc in gpu['procs']:
+                if proc['type'] == 'C':
+                    num_compute_procs += 1
+                elif proc['type'] == 'G':
+                    num_graphics_procs += 1
+                else:
+                    raise NotImplementedError(proc['type'])
+            gpu['num_compute_procs'] = num_compute_procs
+            gpu['num_graphics_procs'] = num_graphics_procs
+        return gpus
 
     if new_mode:
-
-        def _query_nvidia_smi(mode, fields):
-            """
-            Runs nvidia smi in query mode
-
-            Args:
-                mode (str): the query cli flag to pass to nvidia-smi
-                fields (List[str]): csv header fields to query
-
-            Returns:
-                List[Dict[str, str]]: parsed csv output
-            """
-            header = ','.join(fields)
-            cmd_fmtstr = 'nvidia-smi --{mode}="{header}" --format=csv,noheader'
-            info = ub.cmd(cmd_fmtstr.format(mode=mode, header=header))
-            if info['ret'] != 0:
-                raise Exception('unable to call nvidia-smi')
-            rows = []
-            for line in info['out'].split('\n'):
-                line = line.strip()
-                if line:
-                    parts = [p.strip() for p in line.split(',')]
-                    row = ub.dzip(fields, parts)
-                    rows.append(row)
-            return rows
-
+        # This is slightly more robust than the old mode, but it also makes
+        # more than one call to nvidia-smi and cannot return information about
+        # graphics processes.
         fields = ['index', 'memory.total', 'memory.used', 'memory.free',
-                  'count', 'name', 'gpu_uuid']
+                  'name', 'gpu_uuid']
         mode = 'query-gpu'
-        gpu_rows = _query_nvidia_smi(mode, fields)
+        try:
+            gpu_rows = _query_nvidia_smi(mode, fields)
+        except Exception as ex:
+            warnings.warn('Problem running nvidia-smi: {!r}'.format(ex))
+            return None
 
         fields = ['pid', 'name', 'gpu_uuid', 'used_memory']
         mode = 'query-compute-apps'
@@ -815,6 +941,9 @@ def gpu_info(new_mode=False):
             gpu['num_compute_procs'] = num_compute_procs
             # gpu['num_graphics_procs'] = num_graphics_procs
     else:
+        # This is the original implementation of this function. It parses the
+        # direct output of nvidia smi, it is prone to failure if the format of
+        # this program's output ever changes.
         try:
             result = ub.cmd('nvidia-smi')
             if result['ret'] != 0:
@@ -876,6 +1005,10 @@ def gpu_info(new_mode=False):
 
         def parse_proc_line(line):
             inner = '|'.join(line.split('|')[1:-1])
+            if 'no running processes found' in inner.lower():
+                # Handle "No running processes found" case in issue #2
+                return None
+
             parts = [p.strip() for p in inner.split(' ')]
             parts = [p for p in parts if p]
 
@@ -907,8 +1040,9 @@ def gpu_info(new_mode=False):
         for line in proc_lines:
             # Give each GPU info on which processes are using it
             proc = parse_proc_line(line)
-            num = proc['gpu_num']
-            gpus[num]['procs'].append(proc)
+            if proc is not None:
+                num = proc['gpu_num']
+                gpus[num]['procs'].append(proc)
 
         for gpu in gpus.values():
             # Let each GPU know how many processes are currently using it
