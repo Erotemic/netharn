b09976e192e9833a55a41ef1f3682c728aa71773
joncrall <erotemic@gmail.com>
2020-02-08 02:04:16 -0500

    wip

--- netharn/examples/cifar.py
+++ netharn/examples/cifar.py
@@ -683,6 +683,9 @@ if __name__ == '__main__':
 
         # This next command requires a bit more compute
         python -m netharn.examples.cifar --gpu=0 --arch=efficientnet-b0 --nice=test_cifar2 --schedule=step-3-6-50 --lr=0.1 --init=cls --batch_size=2718
+
+        python -m netharn.examples.cifar --gpu=0 --arch=efficientnet-b0 --nice=test_cifar2 --schedule=step-3-3 --lr=0.1 --init=cls --batch_size=2718 --workers=2
+
         python -m netharn.examples.cifar --gpu=0 --arch=efficientnet-b0 --nice=test_cifar2 --schedule=ReduceLROnPlateau-p3-c6 --lr=0.1 --init=cls --batch_size=2719 --workers=4
 
         python -m netharn.examples.cifar.py --gpu=0 --arch=densenet121
