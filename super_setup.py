9d841d51c25fdd44fd8f6edd427e91e801621095
joncrall <erotemic@gmail.com>
2020-04-08 11:26:48 -0400

    wip

--- super_setup.py
+++ super_setup.py
@@ -698,7 +698,7 @@ def make_netharn_registry():
         #     remotes={'public': 'git@gitlab.kitware.com:computer-vision/kwannot.git'},
         # ),
         CommonRepo(
-            name='kwcoco', branch='dev/0.0.5', remote='public',
+            name='kwcoco', branch='dev/0.1.1', remote='public',
             remotes={'public': 'git@gitlab.kitware.com:computer-vision/kwcoco.git'},
         ),
         CommonRepo(
