f8f05618af546a96b711b2100d9377fdacff93be
joncrall <jon.crall@kitware.com>
2020-04-14 12:31:59 -0400

    hotfix verison number 0.5.6

--- netharn/__init__.py
+++ netharn/__init__.py
@@ -4,7 +4,7 @@
 mkinit netharn --noattrs --dry
 mkinit netharn --noattrs
 """
-__version__ = '0.5.5'
+__version__ = '0.5.6'
 
 try:
     # PIL 7.0.0 removed PIL_VERSION, which breaks torchvision, monkey patch it
