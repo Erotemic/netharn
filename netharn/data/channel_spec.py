cc88bcc7c2ae9a184ad8326f964850cb1100248b
joncrall <erotemic@gmail.com>
2020-10-21 20:03:22 -0400

    ChannelSpec helpers

--- netharn/data/channel_spec.py
+++ netharn/data/channel_spec.py
@@ -166,6 +166,46 @@ class ChannelSpec(ub.NiceRepr):
         for spec in stream_specs:
             yield spec
 
+    def streams(self):
+        """
+        Breaks this spec up into one spec for each early-fused input stream
+        """
+        streams = [self.__class__(spec) for spec in self.keys()]
+        return streams
+
+    def difference(self, other):
+        """
+        Set difference
+
+        Example:
+            >>> self = ChannelSpec('rgb|disparity,flowx|flowy')
+            >>> other = ChannelSpec('rgb')
+            >>> self.difference(other)
+            >>> other = ChannelSpec('flowx')
+            >>> self.difference(other)
+        """
+        assert len(list(other.keys())) == 1, 'can take diff with one stream'
+        other_norm = ub.oset(ub.peek(other.normalize().values()))
+        self_norm = self.normalize()
+
+        new_streams = []
+        for key, parts in self_norm.items():
+            new_parts = ub.oset(parts) - ub.oset(other_norm)
+            # shrink the representation of a complex r|g|b to an alias if
+            # possible.
+            # TODO: make this more efficient
+            for alias, alias_spec in self._known.items():
+                alias_parts = ub.oset(alias_spec.split('|'))
+                index = subsequence_index(new_parts, alias_parts)
+                if index is not None:
+                    oset_delitem(new_parts, index)
+                    oset_insert(new_parts, index.start, alias)
+            new_stream = '|'.join(new_parts)
+            new_streams.append(new_stream)
+        new_spec = ','.join(new_streams)
+        new = self.__class__(new_spec)
+        return new
+
     def sizes(self):
         """
         Number of dimensions for each fused stream channel
@@ -244,12 +284,12 @@ class ChannelSpec(ub.NiceRepr):
         stream.
 
         Args:
-            item (dict): a batch item
+            item (Dict[str, Tensor]): a batch item containing unfused parts
             axis (int, default=0): concatenation dimension
 
         Returns:
-            Dict[str, Tensor]: mapping between input stream and its early fused
-                tensor input.
+            Dict[str, Tensor]:
+                mapping between input stream and its early fused tensor input.
 
         Example:
             >>> import torch
@@ -284,6 +324,9 @@ class ChannelSpec(ub.NiceRepr):
         """
         break an early fused item into its components
 
+        Args:
+            inputs (Dict[str, Tensor]): dictionary of components
+
         Example:
             >>> import torch
             >>> dims = (4, 4)
@@ -313,6 +356,133 @@ class ChannelSpec(ub.NiceRepr):
                 idx1 = idx2
         return components
 
+    def component_indices(self, axis=2):
+        """
+        Look up component indices within fused streams
+
+        Example:
+            >>> import torch
+            >>> dims = (4, 4)
+            >>> inputs = ['flowx', 'flowy', 'disparity']
+            >>> self = ChannelSpec('disparity,flowx|flowy')
+            >>> inputs = self.component_indices(components)
+            >>> from netharn.data import data_containers
+            >>> item = {k: data_containers.ItemContainer(v, stack=True)
+            >>>         for k, v in inputs.items()}
+            >>> batch = data_containers.container_collate([item, item])
+            >>> components = self.decode(batch)
+        """
+        parsed = self.parse()
+        component_indices = dict()
+        for key, parts in parsed.items():
+            idx1 = 0
+            for part in parts:
+                size = self._size_lut.get(part, 1)
+                idx2 = idx1 + size
+                index = ([slice(None)] * axis + [slice(idx1, idx2)])
+                idx1 = idx2
+                component_indices[part] = (key, index)
+        return component_indices
+
+
+def subsequence_index(oset1, oset2):
+    """
+    Returns a slice into the first items indicating the position of
+    the second items if they exist.
+
+    This is a variant of the substring problem.
+
+    Returns:
+        None | slice
+
+    Example:
+        >>> oset1 = ub.oset([1, 2, 3, 4, 5, 6])
+        >>> oset2 = ub.oset([2, 3, 4])
+        >>> index = subsequence_index(oset1, oset2)
+        >>> assert index
+
+        >>> oset1 = ub.oset([1, 2, 3, 4, 5, 6])
+        >>> oset2 = ub.oset([2, 4, 3])
+        >>> index = subsequence_index(oset1, oset2)
+        >>> assert not index
+    """
+    if len(oset2) == 0:
+        base = 0
+    else:
+        item1 = oset2[0]
+        try:
+            base = oset1.index(item1)
+        except (IndexError, KeyError):
+            base = None
+
+    index = None
+    if base is not None:
+        sl = slice(base, base + len(oset2))
+        subset = oset1[sl]
+        if subset == oset2:
+            index = sl
+    return index
+
+
+def oset_insert(self, index, obj):
+    """
+    self = ub.oset()
+    oset_insert(self, 0, 'a')
+    oset_insert(self, 0, 'b')
+    oset_insert(self, 0, 'c')
+    oset_insert(self, 1, 'd')
+    oset_insert(self, 2, 'e')
+    oset_insert(self, 0, 'f')
+    """
+    if obj not in self:
+        # Bump index of every item after the insert position
+        for key in self.items[index:]:
+            self.map[key] = self.map[key] + 1
+        self.items.insert(index, obj)
+        self.map[obj] = index
+
+
+def oset_delitem(self, index):
+    """
+    for ubelt oset, todo contribute back to luminosoinsight
+
+    >>> self = ub.oset([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    >>> index = slice(3, 5)
+    >>> oset_delitem(self, index)
+
+    self = ub.oset(['r', 'g', 'b', 'disparity'])
+    index = slice(0, 3)
+    oset_delitem(self, index)
+
+    """
+    if isinstance(index, slice) and index == ub.orderedset.SLICE_ALL:
+        self.clear()
+    else:
+        if ub.orderedset.is_iterable(index):
+            to_remove = [self.items[i] for i in index]
+        elif isinstance(index, slice) or hasattr(index, "__index__"):
+            to_remove = self.items[index]
+        else:
+            raise TypeError("Don't know how to index an OrderedSet by %r" % index)
+
+        if isinstance(to_remove, list):
+            # Modified version of discard slightly more efficient for multiple
+            # items
+            remove_idxs = sorted([self.map[key] for key in to_remove], reverse=True)
+
+            for key in to_remove:
+                del self.map[key]
+
+            for idx in remove_idxs:
+                del self.items[idx]
+
+            for k, v in self.map.items():
+                # I think there is a more efficient way to do this?
+                num_after = sum(v >= i for i in remove_idxs)
+                if num_after:
+                    self.map[k] = v - num_after
+        else:
+            self.discard(to_remove)
 
 if __name__ == '__main__':
     """
